0e0491e74a2121efa1b2572811cc0ada73f201b6
castle <gitdlee@gmail.com>
2020-11-02 23:39:51 +0900

    create database is connection

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -7,32 +8,25 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extends: true}));
 
-app.get('/api/customers', (req, res) =>{
-   res.send([
-      {
-         'id': 1,
-         'image': 'https://placeimg.com/64/64/1',
-         'name': '홍길동',
-         'birthday': '961222',
-         'gender': '남자',
-         'job': '대학생'
-      },{
-         'id': 2,
-         'image': 'https://placeimg.com/64/64/2',
-         'name': '이선빈',
-         'birthday': '951223',
-         'gender': '여자',
-         'job': '프로그래머'
-      },{
-         'id': 3,
-         'image': 'https://placeimg.com/64/64/3',
-         'name': '이빈',
-         'birthday': '970224',
-         'gender': '여자',
-         'job': '편집자'
-      }
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
 
-   ]);
+const connection = mysql.createConnection({
+   host: conf.host,
+   user: conf.user,
+   password: conf.password,
+   port: conf.port,
+   database: conf.database,
+});
+connection.connect();
+
+app.get('/api/customers', (req, res) =>{
+   connection.query(
+     "SELECT * FROM CUSTOMER",
+       (err,rows, fields) =>{res.send(rows);
+     }
+   );
 });
 
 app.listen(port,()=> console.log(`Listening on port ${port}`));
